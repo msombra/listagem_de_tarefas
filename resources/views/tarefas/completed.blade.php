fd0aaa6bcbe210eb448a0ccbf5eda31836dd7d92
Matheus Sombra <msombra10@gmail.com>
2023-07-07 10:46:41 -0300

    Adicionado uma mensagem de lista vazia caso não tenham nenhuma tarefa cadastrada ou concluída

--- resources/views/tarefas/completed.blade.php
+++ resources/views/tarefas/completed.blade.php
@@ -46,30 +46,36 @@
         {{-- LISTAGEM --}}
         <div class="row">
             <ul class="list-group">
-                @foreach ($tarefas as $tarefa)
+                @if ($tarefas->count() == 0)
                     <li class="list-group-item d-flex justify-content-between align-items-start hover-item">
-                        {{-- <div class="">
-                            <input class="form-check-input border-black" type="checkbox" value="" id="concluido">
-                        </div> --}}
-                        <div class="ms-2 me-auto">
-                        <div class="fw-bold"><span>{{ ucfirst($tarefa->titulo) }}</span></div>
-                        <small style="cursor:default;"><i>{{ $tarefa->descricao }}</i></small>
-                        </div>
-                        <div>
-                            <div class="">
-                                <form action="{{ route('tarefas.destroy', $tarefa->id) }}" method="post">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger" title="Deletar tarefa concluída">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                        </svg>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
+                        Nenhuma tarefa encontrada.
                     </li>
-                @endforeach
+                @else
+                    @foreach ($tarefas as $tarefa)
+                        <li class="list-group-item d-flex justify-content-between align-items-start hover-item">
+                            {{-- <div class="">
+                                <input class="form-check-input border-black" type="checkbox" value="" id="concluido">
+                            </div> --}}
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold"><span>{{ ucfirst($tarefa->titulo) }}</span></div>
+                            <small style="cursor:default;"><i>{{ $tarefa->descricao }}</i></small>
+                            </div>
+                            <div>
+                                <div class="">
+                                    <form action="{{ route('tarefas.destroy', $tarefa->id) }}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger" title="Deletar tarefa concluída">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+                @endif
             </ul>
         </div>
         {{-- PAGINAÇÃO --}}
--- resources/views/tarefas/index.blade.php
+++ resources/views/tarefas/index.blade.php
@@ -46,47 +46,53 @@
         {{-- LISTAGEM --}}
         <div class="row">
             <ul class="list-group">
-                @foreach ($tarefas as $tarefa)
+                @if ($tarefas->count() == 0)
                     <li class="list-group-item d-flex justify-content-between align-items-start hover-item">
-                        {{-- <div class="">
-                            <input class="form-check-input border-black" type="checkbox" value="" id="concluido">
-                        </div> --}}
-                        <div class="ms-2 me-auto">
-                        <div class="fw-bold"><span>{{ ucfirst($tarefa->titulo) }}</span></div>
-                        <small style="cursor:default;"><i>{{ $tarefa->descricao }}</i></small>
-                        </div>
-                        <div class="d-flex justify-content-center gap-2">
-                            {{-- BOTÃO CONCLUIR --}}
-                            <div class="">
-                                <a href="{{ route('tarefas.completed', $tarefa->id) }}" class="btn btn-success" title="Marcar como concluída">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-                                    </svg>
-                                </a>
-                            </div>
-                            {{-- BOTÃO EDITAR --}}
-                            <div class="">
-                                <a href="{{ route('tarefas.edit', $tarefa->id) }}" class="btn btn-warning" title="Editar tarefa">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                                    </svg>
-                                </a>
-                            </div>
-                            {{-- BOTÃO DELETAR --}}
-                            <div class="">
-                                <form action="{{ route('tarefas.destroy', $tarefa->id) }}" method="post">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger" title="Excluir tarefa">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                                        </svg>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
+                        Nenhuma tarefa encontrada.
                     </li>
-                @endforeach
+                @else
+                    @foreach ($tarefas as $tarefa)
+                        <li class="list-group-item d-flex justify-content-between align-items-start hover-item">
+                            {{-- <div class="">
+                                <input class="form-check-input border-black" type="checkbox" value="" id="concluido">
+                            </div> --}}
+                            <div class="ms-2 me-auto">
+                            <div class="fw-bold"><span>{{ ucfirst($tarefa->titulo) }}</span></div>
+                            <small style="cursor:default;"><i>{{ $tarefa->descricao }}</i></small>
+                            </div>
+                            <div class="d-flex justify-content-center gap-2">
+                                {{-- BOTÃO CONCLUIR --}}
+                                <div class="">
+                                    <a href="{{ route('tarefas.completed', $tarefa->id) }}" class="btn btn-success" title="Marcar como concluída">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                {{-- BOTÃO EDITAR --}}
+                                <div class="">
+                                    <a href="{{ route('tarefas.edit', $tarefa->id) }}" class="btn btn-warning" title="Editar tarefa">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                {{-- BOTÃO DELETAR --}}
+                                <div class="">
+                                    <form action="{{ route('tarefas.destroy', $tarefa->id) }}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="btn btn-danger" title="Excluir tarefa">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+                @endif
             </ul>
         </div>
         {{-- PAGINAÇÃO --}}
@@ -101,7 +107,11 @@
                 </a>
             </div>
             <div class="d-flex justify-content align-items-center gap-3">
-                <small><b>Quantidade:</b> {{ $qtd_tarefas }} tarefas</small>
+                @if ($qtd_tarefas > 1)
+                    <small><b>Quantidade:</b> {{ $qtd_tarefas }} tarefas</small>
+                @else
+                    <small><b>Quantidade:</b> {{ $qtd_tarefas }} tarefa</small>
+                @endif
                 {{ $tarefas->links() }}
             </div>
         </div>
